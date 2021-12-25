8735b90bbd364f3d1d2dec021b6dc91420db46ae
danieljoseph18 <88494797+danieljoseph18@users.noreply.github.com>
2021-12-25 12:57:33 +0000

    Update equalSidesOfArray.js

--- equalSidesOfArray.js
+++ equalSidesOfArray.js
@@ -1,3 +1,6 @@
+//returns index of the point in the array where both sides of it are equal in value, if not returns -1
+// e.g for [1,2,3,4,3,2,1] returns index 3
+
 function findEvenIndex(arr)
 {
   const sum = arr.reduce((a,b) => a+b);
